c16442cbdab08accc36ad418f382ecaaeed3469c
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-03 04:39:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,17 +13,12 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  check = []
-  array.each do |word|
-    word.size <= 4 ? check << false : check << true
-  end
-
-  if check.include?(true)
-    return true
-  else
-    return false
+  array.select do |word|
+    word.size <= 4
   end
 end
+
+
 
 def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
